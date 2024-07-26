8e68df157192d3759879f310987446b0b67916c6
Erfan Rezaei <erfan.rezaei.dev@gmail.com>
2024-07-26 22:07:20 +0330

    Uploading images in articles with CKEditor done

---
+++ Blog/Blog/Areas/Admin/Controllers/UploadController.cs
@@ -0,0 +1,28 @@
+﻿using CoreLayer.Services.FileManager;
+using CoreLayer.Utilities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Areas.Admin.Controllers
+{
+    public class UploadController : Controller
+    {
+        private readonly IFileManager _FileManager;
+
+        public UploadController(IFileManager fileManager)
+        {
+            _FileManager = fileManager;
+        }
+
+        [Route("/Upload/Article")]
+        [Authorize]
+        public IActionResult UploadArticleImage(IFormFile upload)
+        {
+            if (upload == null) return BadRequest();
+
+            var imageName = _FileManager.SaveFile(upload, Directories.PostContentImage);
+
+            return Json(new { Uploaded = true, url = Directories.GetPostContentImage(imageName) });
+        }
+    }
+}
--- Blog/CoreLayer/Utilities/Directories.cs
+++ Blog/CoreLayer/Utilities/Directories.cs
@@ -4,7 +4,11 @@
     {
         public const string PostImage = "wwwroot/images/posts";
 
+        public const string PostContentImage = "wwwroot/images/posts/content";
+
         public static string GetPostImage(string imageName) => $"{PostImage.Replace("wwwroot", "")}/{imageName}";
+
+        public static string GetPostContentImage(string imageName) => $"{PostContentImage.Replace("wwwroot", "")}/{imageName}";
 
     }
 }
